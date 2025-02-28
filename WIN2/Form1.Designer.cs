9b99ae6963dbc550caba731b4d1daa0b4e56c3be
CamiloCT3105 <Usuario@B6-603-37781>
2025-02-28 13:12:53 -0500

    Agregar archivos de proyecto.

---
+++ WIN2/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace APLICACION_BROMA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTN_ACEPTAR = new System.Windows.Forms.Button();
+            this.BTN_CANCEL = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BTN_ACEPTAR
+            // 
+            this.BTN_ACEPTAR.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_ACEPTAR.Location = new System.Drawing.Point(35, 173);
+            this.BTN_ACEPTAR.Name = "BTN_ACEPTAR";
+            this.BTN_ACEPTAR.Size = new System.Drawing.Size(889, 91);
+            this.BTN_ACEPTAR.TabIndex = 0;
+            this.BTN_ACEPTAR.Text = "ACEPTAR";
+            this.BTN_ACEPTAR.UseVisualStyleBackColor = true;
+            this.BTN_ACEPTAR.Click += new System.EventHandler(this.BTN_ACEPTAR_Click);
+            // 
+            // BTN_CANCEL
+            // 
+            this.BTN_CANCEL.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_CANCEL.Location = new System.Drawing.Point(364, 365);
+            this.BTN_CANCEL.Name = "BTN_CANCEL";
+            this.BTN_CANCEL.Size = new System.Drawing.Size(400, 91);
+            this.BTN_CANCEL.TabIndex = 1;
+            this.BTN_CANCEL.Text = "CANCELAR";
+            this.BTN_CANCEL.UseVisualStyleBackColor = true;
+            this.BTN_CANCEL.MouseHover += new System.EventHandler(this.BTN_CANCEL_MouseHover);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("SansSerif", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label1.Location = new System.Drawing.Point(85, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(839, 56);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Realmente desea formatear su disco?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(971, 517);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BTN_CANCEL);
+            this.Controls.Add(this.BTN_ACEPTAR);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTN_ACEPTAR;
+        private System.Windows.Forms.Button BTN_CANCEL;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WIN2/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APLICACION_BROMA
+{
+    public partial class Form1 : Form
+    {
+        int x = 417;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BTN_ACEPTAR_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("FORMATEANDO SU DISCO DURO");
+        }
+
+        private void BTN_CANCEL_MouseHover(object sender, EventArgs e)
+        {
+            x = x + 200;
+
+            BTN_CANCEL.Location = new Point(x, 224);
+        }
+    }
+
+}
